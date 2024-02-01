33c25822a5094e028d626386f7b8ef37c2b2b054
jakub.korous <kubak.ko@seznam.cz>
2024-02-01 13:42:51 +0100

    add 2,3,4 section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: [
+    "./index.html"
+  ],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    extend: {
+      backgroundImage: {
+        'graphic-mob': "url('images/image-cherry-mob.jpg')",
+        'graphic-desk': "url('images/image-cherry-desk.jpg')",
+        'photography-mob': "url('images/image-orange-mob.jpg')",
+        'photography-desk': "url('images/image-orange-desk.jpg')",
+      },
+      screens: {
+        'custom-sm': '530px',
+        'custom-padding': '661px',
+        'custom-md': '992px',
+        'custom-lg': '1440px',
+      },
+      fontFamily: {
+        'custom-heading': ['"Fraunces"', 'serif'],
+        'custom-body': ['"Barlow"', 'sans-serif'],
+      },
+      letterSpacing: {
+        'custom-heading' : '6px',
+        'custom-heading-big' : '9px',
+        'custom-body' : '3px',
+      },
+      height: {
+        'custom-0' : '94vh',
+        'custom-1' : '32rem',
+        'custom-2' : '37rem',
+        'custom-3' : '20rem',
+      },
+      paddingTop: {
+        'custom-1' : '22rem',
+      },
+      colors: {
+        'custom-light-grey' : 'hsl(210, 4%, 67%)',
+        'custom-moderate-grey' : 'hsl(232, 10%, 55%)',
+        'custom-dark-grey' : 'hsl(213, 9%, 39%)',
+        'custom-very-dark-grey' : 'hsl(212, 27%, 19%)',
+        'custom-light-cyan' : 'hsl(167, 51%, 73%)',
+        'custom-moderate-cyan' : 'hsl(168, 34%, 41%)',
+        'custom-moderate-cyan-2' : 'hsl(168, 45%, 32%)',
+        'custom-dark-cyan' : ' hsl(167, 40%, 24%)',
+        'custom-soft-red' : ' hsl(7, 97%, 76%)',
+        'custom-yellow' : '  hsl(51, 89%, 75%)',
+        'custom-dark-blue' : '  hsl(198, 62%, 26%)',
+      },
     },
-   
   },
   plugins: [],
 }
-
-
